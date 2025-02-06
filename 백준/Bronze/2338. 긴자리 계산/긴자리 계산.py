39f690ae25a9c9babd2e7da8ed1c7fe1b3f1d77f
정명우 <163850138+tccmw@users.noreply.github.com>
2025-02-06 17:02:56 +0900

    [Bronze V] Title: 긴자리 계산, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+a = int(input())
+b = int(input())
+
+print(add(a, b))
+print(subtract(a, b))
+print(multiply(a, b))
