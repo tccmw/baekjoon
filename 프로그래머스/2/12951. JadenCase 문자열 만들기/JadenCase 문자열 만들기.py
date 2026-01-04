28a3b2237e9026f1529c906a1bd6221af91de105
정명우 <163850138+tccmw@users.noreply.github.com>
2026-01-04 23:15:50 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.02 ms, Memory: 9.27 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,15 +1,2 @@
 def solution(s):
-    result = []
-    new_word = True
-    for c in s:
-        if c == " ":
-            result.append(c)
-            new_word = True
-        else:
-            if new_word:
-                result.append(c.upper()) 
-            else:
-                result.append(c.lower())
-            new_word = False
-    answer = "".join(result)
-    return answer
+    return ' '.join([word.capitalize() for word in s.split(" ")])
